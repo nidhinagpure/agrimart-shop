6c86586d6acae41b1208ccfbb8d6e45434c55ba2
nidhinagpure <nagpurenidhi8@gmail.com>
2026-05-12 13:27:19 +0530

    first commit

---
+++ backend/server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Backend server running");
+});
+
+app.get("/api/data", (req, res) => {
+  res.json({
+    message: "Hello from backend",
+  });
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
