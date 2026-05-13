c6f6f1a8084a4542c681e4cc08434d03e9f46860
nidhinagpure <nagpurenidhi8@gmail.com>
2026-05-13 14:08:51 +0530

    added naming of footer

--- frontend/src/component/Footer.jsx
+++ frontend/src/component/Footer.jsx
@@ -1,15 +1,90 @@
 import React from 'react'
+import Facebook from '../assets/facebook.png'
+import Twitter from '../assets/twitter.png'
+import Instagra from '../assets/instagram.png'
+import Email from '../assets/email.png'
+import Location from '../assets/location.png'
+import { href } from 'react-router'
+
+const Footer = () => {
+        
+    const socialMediaLinks = [
+        {
+            name: Facebook,
+            alt: 'Facebook',
+            url: 'https://www.facebook.com/'
+        },
+        {
+            name: Twitter,
+            alt: 'Twitter',
+            url: 'https://www.twitter.com/'
+        },
+        {
+            name: Instagra,
+            alt: 'Instagram',
+            url: 'https://www.instagram.com/'
+        },
+        {
+            name: Email,
+            alt: 'Email',
+            url: 'mailto:info@agrimart.com'
+        }
+    ]
+
+    const contactDetails = [
+        {
+            icon: Location,
+            alt : 'Argimart Pune',
+            href: 'https://maps.app.goo.gl/PjnAu7sv8ukzt1xb8',
+        },
+
+        {    
+            icon: Contact,
+            alt : 'Contact Us',
+            href: 'https://maps.app.goo.gl/PjnAu7sv8ukzt1xb8',
+        },
+
+        {
+            icon: Email,
+            alt : 'Gmail Us',
+            href: 'mailto:info@agrimart.com',
+        }
+
+    ],
+
+    const services = [
+        { name: 'Essentials', href: '/essentials', },
+        { name: 'Vegetables', href: '/essentials', },
+        { name: 'Fruits', href: '/essentials', },
+        { name: 'Exotics', href: '/essentials', },
+        { name: 'Dairy', href: '/essentials', }
+    ],
+
+    const quickLinks = [
+        { name: 'Home', path: '/Home', },
+        { name: 'Services', path: '/Home', },
+        { name: 'About Us', path: '/services' },
+        { name: 'Contact Us', path: '/Home', },
+        { name: 'feedback', path: '/services' },
+    ]
 
 
 
-function Footer() {
+
+
+
   return (
     <>
-    <div className='my-30'>
-        <p>&copy; 2023 Your Company. All rights reserved.</p>
-    </div>
+    <footer>
+       <div>
+        
+       </div>
+    </footer>
+    
+
     </>
   )
 }
 
 export default Footer
+
