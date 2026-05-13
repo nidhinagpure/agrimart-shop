20f4ce35c86338eac4e61c215ec8115dd01801bd
nidhinagpure <nagpurenidhi8@gmail.com>
2026-05-14 00:05:28 +0530

    fix styling of footer

--- frontend/src/component/Footer.jsx
+++ frontend/src/component/Footer.jsx
@@ -13,11 +13,6 @@ const Footer = () => {
         
     const socialMediaLinks = [
         {
-            name: Facebook,
-            alt: 'Facebook',
-            url: 'https://www.facebook.com/'
-        },
-        {
             name: Twitter,
             alt: 'Twitter',
             url: 'https://www.twitter.com/'
@@ -74,58 +69,73 @@ const Footer = () => {
   return (
     <>
     <footer>
-       <div className='flex bg-green-800'>
+       <div className='flex bg-green-800 justify-between gap-30 px-6 py-16 pb-34 mb-0 mt-20 w-full'>
+ 
+        <div className=''>
+            <span><img src={Logo} className='w-60 ' alt="Logo" /></span>
+            <p className='w-60 line-clamp-10 text-left ml-6'>
+            Discover pure, organic goodness at Agrimart where fresh, wholesome products come straight from nature to you.</p>
+        </div>
 
-          <div className=''>
-           <span><img src={Logo} className='w-64' alt="Logo" /> </span>
-           <p className='w-50'>Discover pure, organic goodness at Agrimart where fresh, wholesome products come straight from nature to you.</p>
-          </div>
+        <div>
+            <h1 className='font-medium text-xl py-1'>Our Services</h1>
+            <ul>
+            {services.map((service,index) => (
+            <li key={index} className='text-md py-1'> 
+                <Link to={service.href}>{service.name}</Link> 
+            </li>
+            ))}
+            </ul>
+        </div>
 
-           <div>
-             <h1 className='font-bold text-lg'>Our Services</h1>
-             <ul>
-                {services.map((service,index) => (
-                <li key={index}> 
-                    <Link to={service.href}>{service.name}</Link> 
+        <div>
+            <h3 className='font-medium text-xl py-1'>Useful Links</h3>
+            <ul>
+            {quickLinks.map((link,index)=>(
+                <li key={index} className='text-md py-1'>
+                    <Link to={link.path}>{link.name} </Link>
                 </li>
-                ))}
-             </ul>
-          </div>
+            ))}
+            </ul>
+        </div>
 
-           <div>
-             <h3>Useful Links</h3>
-             <ul>
-                {quickLinks.map((link,index)=>(
-                    <li key={index}>
-                        <Link to={link.path}> {link.name} </Link>
-                    </li>
-                ))}
-             </ul>
-          </div>
+        <div className=''>
+            <h3 className='font-medium text-xl py-1'>Contact Us</h3>
+            <ul className=''>
+            {contactDetails.map((contact,index) => (
+                <li key={index}>
+                 <div className='flex py-1'>
+                   <img src={contact.icon} className='w-6 h-6' alt={contact.alt}/>
+                    <a href={contact.href} target="_blank" rel="noopener noreferrer">  <p className='px-3'>{contact.alt} </p></a>
+                 </div>    
+                </li>
+            ))}
+            </ul>
+        </div>
 
-           <div>
-             <h3>Contact Us</h3>
-             <ul>
-                {contactDetails.map((contact,index) => (
-                    <li key={index}>
-                         <img src={contact.icon} alt={contact.alt}/><a href={contact.href} target="_blank" rel="noopener noreferrer">
-                            {contact.alt}
-                         </a>
-                    </li>
+        <div className='gap-8 pr-6'>
+        <h3 className='font-medium text-xl py-1'>Follow Us</h3>
+        <div className='flex gap-4 py-1'>
+            {socialMediaLinks.map((social,index) => (
+            <a href={social.url} key={index} target="_blank" rel="noopener noreferrer"> 
+               <img src={social.name} className='w-6 h-6' alt={social.alt} /></a>
                 ))}
-             </ul>
-          </div>
-
-          <div>
-                <h3> Follow Us</h3>
-                {socialMediaLinks.map((social,index) => (
-                <a href={social.url} target="_blank" rel="noopener noreferrer"> 
-                    <img src={social.name} alt={social.alt} />
-                </a>
-                ))}
-          </div>
-          
+        </div>
+      </div>    
+  
        </div>
+
+
+         <div className='w-7xl bg-gray-300 items-center mx-auto py-2 flex justify-between px-12 mt-[-6%]'>
+          <p>© 2024 All rights reserved Agrimart</p>
+          <div className='flex gap-4'>
+          {socialMediaLinks.map((social,index)=>(
+                <a href={social.url} key={index} target="_blank" rel="noopener noreferrer">
+                    <img  src={social.name} className='w-6 h-6' alt={social.alt} />
+                </a> 
+            ))}
+            </div>
+         </div>
 
     </footer>
     </>
